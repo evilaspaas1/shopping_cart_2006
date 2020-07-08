7f4a2d790cfff100b897b8d3c0d4b46b5d0ef4eb
Austin Aspaas <araspaas@live.com>
2020-07-08 09:15:44 -0600

    Add is hoarded test

--- test/product_test.rb
+++ test/product_test.rb
@@ -37,4 +37,10 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_product_is_hoarded
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+    assert_equal false, product.is_hoarded?
+  end
 end
