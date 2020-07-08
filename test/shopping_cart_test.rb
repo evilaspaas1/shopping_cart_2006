3156424f0972c0966bab0487486d0a8f3a511025
Austin Aspaas <araspaas@live.com>
2020-07-08 10:17:03 -0600

    Add is full test

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -41,4 +41,13 @@ class ShoppingCartTest < Minitest::Test
     @cart.add_product(@product3)
     assert_equal 13, @cart.total_number_of_products
   end
+
+  def test_it_is_full
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+    assert_equal false, @cart.is_full?
+    @cart.add_product(@product4)
+    assert_equal true, @cart.is_full?
+  end
 end
