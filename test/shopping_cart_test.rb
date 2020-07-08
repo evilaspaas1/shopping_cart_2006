47e8a86432fb2f61a9f4151b179d84949b8ff1cf
Austin Aspaas <araspaas@live.com>
2020-07-08 10:35:20 -0600

    Add percentage occupied test

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -58,4 +58,11 @@ class ShoppingCartTest < Minitest::Test
     @cart.add_product(@product4)
     assert_equal [@product1, @product3], @cart.products_by_category(:paper)
   end
+
+  def test_it_can_calculate_percentage_occupied
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+    assert_equal 43.33, @cart.percentage_occupied
+  end
 end
